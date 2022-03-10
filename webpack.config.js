281c83ffc89bc3b8cfe38ba9b5d5fcb98780f9a9
waiwai2525 <waiwai.cps@gmail.com>
2022-03-10 13:52:56 +0900

    chore: Add eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-    output: {
-        clean: true,
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: [".ts", ".js", ".wasm"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.html")
-        }),
-        new WasmPackPlugin({
-            crateDirectory: __dirname
-        })
+  output: {
+    clean: true,
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js", ".wasm"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
     ],
-    experiments: {
-        asyncWebAssembly: true
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html"),
+    }),
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
